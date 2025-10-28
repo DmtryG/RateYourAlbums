caf4c414399cdbe02bbdaa7d6cbbe2f2a53f6da9
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-10-28 14:37:36 +0500

    Initial Commit

---
+++ RateYourAlbums/Models/Album.swift
@@ -0,0 +1,9 @@
+//
+//  Album.swift
+//  RateYourAlbums
+//
+//  Created by Dmitriy Guryanov on 28.10.2025.
+//
+
+import Foundation
+import SwiftData
---
+++ RateYourAlbums/RateYourAlbumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateYourAlbumsApp.swift
+//  RateYourAlbums
+//
+//  Created by Dmitriy Guryanov on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RateYourAlbumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
