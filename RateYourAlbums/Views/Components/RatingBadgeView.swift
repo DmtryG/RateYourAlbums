db612f1b2d78e12f2a52e14feaca4c71f7cd6157
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-10-29 01:17:21 +0500

    Implemented RatingBadgeView

---
+++ RateYourAlbums/Views/Components/RatingBadgeView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingBadgeView.swift
+//  RateYourAlbums
+//
+//  Created by Dmitriy Guryanov on 29.10.2025.
+//
+
+import SwiftUI
+
+struct RatingBadgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingBadgeView()
+}
