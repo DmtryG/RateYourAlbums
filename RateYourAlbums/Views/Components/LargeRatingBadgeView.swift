ce11482bc23a92e54bccc7be9d12fc12cbfe4509
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-10-29 11:06:05 +0500

    Implemented LargeRatingBadgeView

---
+++ RateYourAlbums/Views/Components/LargeRatingBadgeView.swift
@@ -0,0 +1,18 @@
+//
+//  LargeRatingBadgeView.swift
+//  RateYourAlbums
+//
+//  Created by Dmitriy Guryanov on 29.10.2025.
+//
+
+import SwiftUI
+
+struct LargeRatingBadgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LargeRatingBadgeView()
+}
