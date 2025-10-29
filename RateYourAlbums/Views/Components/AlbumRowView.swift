d3cadd6c8b576255fc0e5348d898aab2e152f43d
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-10-29 12:09:08 +0500

    Implemented AlbumRowView

---
+++ RateYourAlbums/Views/Components/AlbumRowView.swift
@@ -0,0 +1,52 @@
+//
+//  AlbumRowView.swift
+//  RateYourAlbums
+//
+//  Created by Dmitriy Guryanov on 29.10.2025.
+//
+
+import SwiftUI
+
+struct AlbumRowView: View {
+    let album: Album
+    var body: some View {
+        HStack (spacing: 12) {
+            AlbumCoverView(
+                artworkURL: album.artworkURL,
+                size: 70,
+                cornerRadius: 18
+            )
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(album.title)
+                    .font(.headline)
+                    .lineLimit(1)
+                Text (album.artistName)
+                    .font(.subheadline)
+                    .foregroundStyle(.red.opacity(0.8))
+                    .lineLimit(1)
+            }
+            
+            Spacer()
+            
+            RatingBadgeView(
+                rating: album.rating,
+                size: 60
+            )
+        }
+        .padding(10)
+        .background(Color(.systemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 18))
+        .shadow(color: .black.opacity(0.1), radius: 8)
+    }
+}
+
+#Preview {
+    AlbumRowView(album: Album(
+        id: "1",
+        title: "Abbey Road",
+        artistName: "The Beatles",
+        artworkURL: nil,
+        rating: 9.5
+    ))
+}
