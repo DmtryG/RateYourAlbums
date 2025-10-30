5c7ebcbe90cbd4ad58a3a91a14c533f25e57485b
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-10-30 06:31:54 +0500

    Implementing AlbumDetailViewModel

--- RateYourAlbums/ViewModels/AlbumDetailViewModel.swift
+++ RateYourAlbums/ViewModels/AlbumDetailViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftData
+
+@Observable
+class AlbumDetailViewModel {
+    var album: Album?
+    var albumDTO: AlbumDTO?
+    var isInLibrary = false
+    var currentRating: Double = 5.0
+    
+    private var modelContext: ModelContext?
+    
+    func setModelContext (_ context: ModelContext) {
+        self.modelContext = context
+    }
+    
+}
