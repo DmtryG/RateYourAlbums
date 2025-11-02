38f75d1ebe0c58fa45029fbe4e0afce731ff783f
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-11-03 02:06:01 +0500

    Started implementing ViewModel for Stats

--- RateYourAlbums/ViewModels/StatsViewModel.swift
+++ RateYourAlbums/ViewModels/StatsViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftData
+
+struct DayContribution: Identifiable {
+    let id = UUID()
+    let date: Date
+    let count: Int
+    
+    var level: Int {
+        switch count {
+        case 0: return 0
+        case 1...2: return 1
+        case 3...4: return 2
+        case 5...6: return 3
+        default: return 4
+        }
+    }
+}
