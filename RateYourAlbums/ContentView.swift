09626811e533ed01ff40199bf18304613dcf56ac
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-12-01 00:48:14 +0500

    Starting from scratch

--- RateYourAlbums/ContentView.swift
+++ RateYourAlbums/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.background
+            VStack {
+                Text("RateYourAlbums")
+                    .foregroundStyle(Color.accent)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .fontWidth(.expanded)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
